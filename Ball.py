9b602cd920b1db43f4b9805375954afefff8a114
Tombosoa <hei.nantenaina.4@gmail.com>
2024-01-28 08:29:35 +0300

    feat: change ball's numbers and radius

--- Ball.py
+++ Ball.py
@@ -10,8 +10,8 @@ WHITE = (255, 255, 255)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-BALL_RADIUS = 50
-NUM_BALLS = 6
+BALL_RADIUS = 10
+NUM_BALLS = 60
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Ball")
